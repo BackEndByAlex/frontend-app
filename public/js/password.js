e81ab8268a30eba4575bf147403b1aaed9c50ce8
Alexandru C.A <aa227wr@student.lnu.se>
2025-04-28 14:01:04 +0200

    password generater funtionality implemententation.

--- public/js/password.js
+++ public/js/password.js
@@ -1,25 +1,21 @@
 import { onDomReady } from './utils.js'
 
 onDomReady(() => {
-  const copyButton = document.getElementById('copy-password')
-  const toggleEyeButton = document.getElementById('toggle-password-visibility')
-  const passwordInput = document.getElementById('generated-password')
+  const copyButton = document.querySelector('#copy-password')
+  const toggleEyeButton = document.querySelector('#toggle-password-visibility')
+  const passwordInput = document.querySelector('#generated-password')
+  const generateButton = document.querySelector('#generate-password')
 
   if (copyButton && passwordInput) {
     copyButton.addEventListener('click', () => {
       navigator.clipboard.writeText(passwordInput.value)
         .then(() => {
-          console.log('Lösenord kopierat!')
-
-          // Gör att öga blinkar
           if (toggleEyeButton) {
             toggleEyeButton.classList.add('blink-animation')
             setTimeout(() => {
               toggleEyeButton.classList.remove('blink-animation')
             }, 600)
           }
-
-          // Ändra kopieringsikonens färg tillfälligt
           copyButton.querySelector('svg').style.color = '#22c55e'
           setTimeout(() => {
             copyButton.querySelector('svg').style.color = ''
@@ -28,6 +24,29 @@ onDomReady(() => {
         .catch(err => {
           console.error('Kunde inte kopiera:', err)
         })
+    })
+  }
+
+  if (generateButton && passwordInput) {
+    generateButton.addEventListener('click', async () => {
+      try {
+        const res = await fetch('/generate-password')
+        const data = await res.json()
+        passwordInput.value = data.password
+        passwordInput.type = 'password'
+      } catch (err) {
+        console.error('Kunde inte generera lösenord:', err)
+      }
+    })
+  }
+
+  if (toggleEyeButton && passwordInput) {
+    toggleEyeButton.addEventListener('click', () => {
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text'
+      } else {
+        passwordInput.type = 'password'
+      }
     })
   }
 })
